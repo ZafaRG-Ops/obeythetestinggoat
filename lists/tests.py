fd3a72c28dc19409d76648adfb88a83b55968e8b
ZAFAR AHMAD AWAN <zafarahmadawan1@gmail.com>
2019-10-22 16:26:11 +0200

    Add app for lists, with deliberately failing unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
-class HomePageTest(TestCase):
+class SmokeTest(TestCase):
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 3)
