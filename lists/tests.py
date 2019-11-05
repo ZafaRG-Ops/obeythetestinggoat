89981bd81d45291039fb11cd9bcd3bf657fca13a
zafar ahmad awan <zafar@zafar.zafar>
2019-11-05 17:05:53 +0100

    upload all

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,30 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-class ItemModelTest(TestCase):
+from lists.views import home_page
 
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
+class HomePageTest(TestCase):
 
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>', html)
+
+	def test_can_save_a_POST_request(self):
+		self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
