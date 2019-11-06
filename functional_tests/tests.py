c731385915853b10c1eabfa4428b6b248a6965dc
zafar ahmad awan <zafar@zafar.zafar>
2019-11-06 16:48:55 +0100

    7.7ok

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -52,19 +52,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Satisfied, she goes back to sleep
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')  
-
+        self.assertRegex(edith_list_url, '/lists/.+')
         # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
@@ -97,8 +95,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to slee
-
-  
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore') 
--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,19 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
+		response = self.client.get('/')
 		html = response.content.decode('utf8')
+
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do lists</title>', html)
+		self.assertTrue(html.strip().endswith('</html>'))
 
 	def test_can_save_a_POST_request(self):
 		self.client.post('/', data={'item_text': 'A new list item'})
@@ -28,3 +25,9 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+class ListViewTest(TestCase):
+
+	def test_user_list_tamplate(self):
+		response = self.client.get('/lists/the-only-list-in-the-woirl/')
+		self.assertTemplateUsed(response, 'list.html')
